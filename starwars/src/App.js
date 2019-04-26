2bebc283043cf560e752c825345beca09870dd3a
mdelzotto <madelzotto@comcast.net>
2019-04-26 12:41:44 -0400

    added traits to the characters from the react app

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -38,7 +38,23 @@ class App extends Component {
         <h1 className="Header">React Wars</h1>
         <ul>
         {this.state.starwarsChars.map((char) => {
-          return <li>{char.name}</li>
+          return <li>
+            <h2>{char.name}</h2>
+            <p>Birth year: {char.birth_year}</p>
+            <p>Created: {char.created}</p>
+            <p>Edited: {char.edited}</p>
+            <p>Eye colour: {char.eye_color}</p>
+            <p>Films: {char.films}</p>
+            <p>Gender: {char.gender}</p>
+            <p>Hair colour: {char.hair_color}</p>
+            <p>Height: {char.height}</p>
+            <p>Homeworld: {char.homeworld}</p>
+            <p>Mass: {char.mass}</p>
+            <p>Skin colour: {char.skin_color}</p>
+            <p>Species: {char.species}</p>
+            <p>URL: {char.url}</p>
+            <p>Vehicles: {char.vehicles}</p>
+          </li>
         })}
         </ul>
       </div>
